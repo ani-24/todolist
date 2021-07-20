2b5437e9a75514ff486ccb2aec38b97a8082d0b0
Aniket <rajeshwarkumar9204@gmail.com>
2021-07-20 20:30:54 +0530

    push-to-list and add to localStorage added

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
 const addBtn = document.querySelector(".add-task-btn");
 const addTask = document.querySelector(".add-task");
+const addInput = document.querySelector(".todo-input");
+
+const todoList = document.querySelector(".todo-list");
+
+
+const addToList = (task) => {
+  if (task !== "") {
+    let storedTask = localStorage.getItem("task");
+    if (!storedTask) {
+      let taskArray = [];
+      taskArray.push(task);
+      localStorage.setItem("task", JSON.stringify(taskArray));
+    } else {
+      storedTask = JSON.parse(localStorage.getItem("task"));
+      storedTask.push(task);
+      localStorage.setItem("task", JSON.stringify(storedTask));
+    }
+    console.log(localStorage.getItem("task"));
+    const item = document.createElement("li");
+    item.classList.add("todo-item");
+    item.innerHTML = `
+    <div class="left-operation">
+    <i class="far fa-circle status"></i>
+    <p class="task">${task}</p>
+    </div>
+    <div class="right-operation">
+    <i class="far fa-edit edit"></i>
+    <i class="far fa-trash-alt delete"></i>
+    </div>
+    `
+    todoList.appendChild(item);
+  }
+}
 
 addBtn.addEventListener("click", () => {
   addTask.classList.toggle("show-input");
   if (addTask.classList.contains("show-input")) {
-    addBtn.setAttribute("style", "transform: rotate(45deg);")
+    addInput.focus();
   } else {
-    addBtn.setAttribute("style", "transform: rotate(0);")
+    addToList(addInput.value);
+    addInput.value = "";
+  }
+});
+
+addInput.addEventListener("keyup", key => {
+  if (key.keyCode == 13) {
+    addToList(addInput.value);
+    addInput.value = "";
+    addTask.classList.remove("show-input");
   }
 })
+
